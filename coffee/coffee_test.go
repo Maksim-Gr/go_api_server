01b5ea6d7b9acace37783631cba9bb0afa54d9cc
Maksim Grishchenko <maksim.g.grishchenko@gmail.com>
2023-08-06 13:22:59 +0300

    add testing

---
+++ coffee/coffee_test.go
@@ -0,0 +1,34 @@
+package coffee
+
+import "testing"
+
+func init() {
+	Coffees = CoffeeList{
+		List: []CoffeeDetails{
+			{"Latte", 2.5},
+			{"Flat White", 2},
+			{"Cappuccino", 2.25},
+		},
+	}
+}
+
+func TestIsCoffeeAvailable(t *testing.T) {
+	type testCase struct {
+		coffeeType string
+		want       bool
+	}
+
+	cases := []testCase{
+		{"lat", false},
+		{"Latte", true},
+		{"", false},
+		{"cappacunio", false},
+	}
+
+	for _, tc := range cases {
+		got := IsCoffeeAvailable(tc.coffeeType)
+		if tc.want != got {
+			t.Errorf("Expected '%v', but got '%v'", tc.want, got)
+		}
+	}
+}
